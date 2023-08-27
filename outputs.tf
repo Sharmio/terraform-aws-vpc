591e3df24329032934e422567b3f3ba342f5cb28
Mohit Sharma <imoisharma@icloud.com>
2023-08-27 16:01:58 +1000

    modified vpc config

--- outputs.tf
+++ outputs.tf
@@ -1,48 +1,50 @@
 # Output the VPC ID
 output "vpc_id" {
   value       = aws_vpc.vpc_poc.id
-  description = "The ID of the VPC"
+  description = "The ID of the VPC."
 }
 
 output "vpc_arn" {
   value       = aws_vpc.vpc_poc.arn
-  description = "Amazon Resource Name (ARN) of VPC"
+  description = "Amazon Resource Name (ARN) of the VPC."
 }
 
 output "instance_tenancy" {
   value       = aws_vpc.vpc_poc.instance_tenancy
-  description = "Tenancy of instances spin up within VPC"
+  description = "The tenancy of instances spun up within the VPC."
 }
 
 output "enable_network_address_usage_metrics" {
   value       = aws_vpc.vpc_poc.enable_network_address_usage_metrics
-  description = "Whether Network Address Usage metrics are enabled for the VPC"
+  description = "Indicates whether Network Address Usage metrics are enabled for the VPC."
 }
 
 output "main_route_table_id" {
   value       = aws_vpc.vpc_poc.main_route_table_id
-  description = "The ID of the main route table associated with this VPC. Note that you can change a VPC's main route table by using an aws_main_route_table_association."
+  description = "The ID of the main route table associated with this VPC. You can change a VPC's main route table using an aws_main_route_table_association."
 }
 
 output "enable_dns_hostnames" {
   value       = aws_vpc.vpc_poc.enable_dns_hostnames
-  description = "Tenancy of instances spin up within VPC"
+  description = "Indicates whether DNS hostnames are enabled for the VPC."
 }
 
-
 output "private_subnet_id" {
-  value = aws_subnet.private_subnets[*].id
+  value       = aws_subnet.private_subnets[*].id
+  description = "The IDs of the private subnets."
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public_subnets[*].id
+  value       = aws_subnet.public_subnets[*].id
+  description = "The IDs of the public subnets."
 }
 
 output "private_subnet_arn" {
-  value = aws_subnet.private_subnets[*].arn
+  value       = aws_subnet.private_subnets[*].arn
+  description = "Amazon Resource Names (ARNs) of the private subnets."
 }
 
 output "public_subnet_arn" {
-  value = aws_subnet.public_subnets[*].arn
+  value       = aws_subnet.public_subnets[*].arn
+  description = "Amazon Resource Names (ARNs) of the public subnets."
 }
-
--- variables.tf
+++ variables.tf
@@ -1,85 +1,84 @@
-
 variable "instance_tenancy" {
   type        = string
-  description = "What type of instance tenancy"
+  description = "The type of instance tenancy for the VPC (default or dedicated)."
   default     = "default"
 }
 
 variable "cidr_block" {
   type        = string
-  description = "Enter the cidr block"
+  description = "The CIDR block for the VPC."
   default     = "10.0.0.0/20"
 }
 
 variable "project" {
   type        = string
-  description = "Your project name or identifier"
+  description = "The name or identifier of your project."
 }
 
 variable "company" {
   type        = string
-  description = "Your company name or identifier"
+  description = "The name or identifier of your company."
 }
 
 variable "branch" {
   type        = string
-  description = "(optional) describe your variable"
+  description = "The branch or environment to deploy the infrastructure to."
 }
 
 variable "terraform_version" {
   type        = string
-  description = "(optional) describe your variable"
+  description = "The version of Terraform to be used."
 }
 
 variable "aws_provider_version" {
   type        = string
-  description = "optional"
+  description = "The version of the AWS provider to be used."
 }
 
 variable "enable_dns_hostnames" {
-  type        = string
-  description = "enable host name for dns"
+  type        = bool
+  description = "Enable DNS hostnames for the VPC."
   default     = true
 }
 
 variable "enable_dns_support" {
-  type        = string
-  description = "enable dns support"
+  type        = bool
+  description = "Enable DNS support for the VPC."
   default     = true
 }
 
 variable "common_resource_tags" {
-  description = "Common tags from providers.tf"
+  description = "Common tags to be applied to resources."
   type        = map(string)
   default     = {}
 }
 
 variable "enable_network_address_usage_metrics" {
-  description = "(Optional) Indicates whether Network Address Usage metrics are enabled for your VPC. Defaults to false."
-  type        = string
+  description = "Indicates whether Network Address Usage metrics are enabled for the VPC."
+  type        = bool
   default     = false
 }
 
 variable "availability_zones" {
   type        = list(string)
-  description = "Enter the Availabilit Zones"
+  description = "The list of Availability Zones for subnet creation."
   default     = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
 }
 
 variable "public_subnet_cidrs" {
   type        = list(string)
-  description = "Public Subnet CIDR values"
+  description = "CIDR blocks for public subnets."
   default     = ["10.0.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
-  description = "Private Subnet CIDR values"
+  description = "CIDR blocks for private subnets."
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "enable_resource_name_dns_a_record_on_launch" {
-  type        = string
-  description = "(Optional) Indicates whether to respond to DNS queries for instance hostnames with DNS A records"
+  type        = bool
+  description = "Indicates whether to respond to DNS queries for instance hostnames with DNS A records."
   default     = false
 }
